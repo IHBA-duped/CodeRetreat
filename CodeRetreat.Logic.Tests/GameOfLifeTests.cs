d7a49ffecb66876bf57746f9b646db552be29461
Ihor Babarykin <IHBA@simcorp.com>
2024-05-22 03:33:25 -0700

    Updated Grid case data

--- CodeRetreat.Logic.Tests/GameOfLifeTests.cs
+++ CodeRetreat.Logic.Tests/GameOfLifeTests.cs
@@ -5,13 +5,22 @@ namespace CodeRetreat.Logic.Tests
     {
         private static readonly bool[,] EmptyGrid = new bool[0, 0];
 
-        private static readonly bool[,] Grid1 =
+        private static readonly bool[,] Grid =
         {
-            { true, true, true, true, true },
-            { true, false, false, false, true },
-            { true, true, false, false, true },
-            { true, false, true, false, true },
-            { true, true, true, true, true }
+            { false, false, false, false, false },
+            { false, true, true, true, false },
+            { false, false, true, true, false },
+            { false, true, false, true, false },
+            { false, false, false, false, false }
+        };
+
+        private static readonly bool[,] GridResult =
+        {
+            { false, false, true, false, false },
+            { false, true, false, true, false },
+            { false, true, false, false, true },
+            { false, false, false, true, false },
+            { false, false, false, false, false }
         };
 
         [Test]
