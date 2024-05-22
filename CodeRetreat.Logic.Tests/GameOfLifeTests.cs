2197aeb646cffa412df1283ba692daee4db830dd
Ihor Babarykin <IHBA@simcorp.com>
2024-05-22 02:29:15 -0700

    It's not much

---
+++ CodeRetreat.Logic.Tests/GameOfLifeTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace CodeRetreat.Logic.Tests;
+
+[TestFixture]
+public class GameOfLifeTests
+{
+    private static readonly bool[,] EmptyGrid = new bool[0, 0];
+
+    private static readonly bool[,] Grid1 =
+    {
+        { true, true, true, true, true },
+        { true, false, false, false, true },
+        { true, true, false, false, true },
+        { true, false, true, false, true },
+        { true, true, true, true, true }
+    };
+
+    private static readonly bool[,] Grid1Result =
+    {
+        { true, true, true, true, true },
+        { true, false, false, false, true },
+        { true, true, false, false, true },
+        { true, true, true, false, true },
+        { true, true, true, true, true }
+    };
+
+    [Test]
+    public void GetGetNextGeneration_EmptyGrid_ReturnsEmptyGrid()
+    {
+        var result = GameOfLife.GetNextGeneration(EmptyGrid);
+
+        Assert.That(StructuralComparisons.StructuralEqualityComparer.Equals(EmptyGrid, result), Is.True);
+    }
+}
---
+++ CodeRetreat.Logic/GameOfLife.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeRetreat.Logic;
+
+public class GameOfLife
+{
+    public static bool[,] GetNextGeneration(bool[,] grid)
+    {
+        return new bool[0,0];
+    }
+}
